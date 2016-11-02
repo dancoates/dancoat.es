fa8ca31182af62f0ae96db5c4e0133a8f76b5d94
Dan Coates <mail@dancoat.es>
2016-11-02 23:07:22 +1100

    Fix schema drop commands

--- database/schema.sql
+++ database/schema.sql
@@ -6,9 +6,9 @@
 -- Accounts
 -----------
 
-drop sequence account_id_sequence;
-drop table account;
-drop trigger update_account_modified;
+drop trigger update_account_modified on account cascade;
+drop sequence account_id_sequence cascade;
+drop table account cascade;
 
 create sequence account_id_sequence;
 create table account(  
@@ -27,16 +27,15 @@ create trigger update_account_modified before update on account for each row exe
 -- Posts
 --------
 
-drop sequence post_id_sequence;
-drop table post;
-drop trigger update_post_modified;
+drop trigger update_post_modified on post cascade;
+drop sequence post_id_sequence cascade;
+drop table post cascade;
 
 create sequence post_id_sequence;
 
 create table post(
     id int default nextval('post_id_sequence') primary key,
     slug text not null,
-    version int not null,
     created timestamp with time zone default now(),
     modified timestamp with time zone default now()
 );
@@ -49,14 +48,15 @@ create trigger update_post_modified before update on post for each row execute p
 -- Post Versions
 ----------------
 
-drop sequence post_version_id_sequence;
-drop table post_version;
-drop trigger update_post_version_modified;
+drop trigger update_post_version_modified on post_version cascade;
+drop sequence post_version_id_sequence cascade;
+drop table post_version cascade;
 
 create sequence post_version_id_sequence;
 
 create table post_version(
     id int default nextval('post_id_sequence') primary key,
+    head boolean not null,
     post int references post on update cascade,
     author int references account on update cascade,
     body text,
@@ -69,11 +69,6 @@ alter sequence post_version_id_sequence owned by post_version.id;
 
 create trigger update_post_version_modified before update on post_version for each row execute procedure update_modified_column();
 
-
--- Post -> Version
-------------------
-
-alter table post add constraint versionfk foreign key (version) references post_version;
 
 
 -- ----------------------
